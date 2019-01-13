ad6dafb1758c41f5527467c14930e1a036e4392e
Jeffrey Meesters <jjeffreymeesters@gmail.com>
2019-01-13 16:03:19 +0100

    create new book get and post route and install body-parser

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,12 @@ router.get('/', (req, res) => {
     res.render('index')
 });
 
+router.get('/new_book', (req, res) => {
+    res.render('new_book')
+});
+
+router.post('/new_book', (req, res) => {
+    console.log(req.params)
+});
+
 module.exports = router;
