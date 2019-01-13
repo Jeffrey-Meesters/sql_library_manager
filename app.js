3c66002e206ba1b9a1fd65ed3f013967b04d3089
Jeffrey Meesters <jjeffreymeesters@gmail.com>
2019-01-13 18:05:53 +0100

    Refactor into requested route naming and file-names

--- app.js
+++ app.js
@@ -8,13 +8,18 @@ app.use(bodyParser.urlencoded({extended: false}));
 const host ='localhost';
 const port = 3000;
 
+// Host all public files via express static
 app.use('/static', express.static('public'));
+// We're using pug so set the view engine to pug
 app.set('view engine', 'pug');
 
+// Because I've made all the routes in .route/index we don't need to specify the path further
 const routes = require('./routes');
-
+// Let app use the routes to start routing
 app.use(routes);
 
+// Call sync on sequelize before starting the server
+// So the Database gets initialized
 sequelize.sync().then(function() {
     app.listen(port, () => {
         console.log(`app is running on: http://${host}:${port}/`);
--- routes/index.js
+++ routes/index.js
@@ -2,52 +2,64 @@ const express = require('express');
 const router = express.Router();
 const Book = require("../models").Book;
 
+// Redirect root route to /books
 router.get('/', (req, res) => {
-    // FindAll accepts an array of arrays as options
-    // This way you can order on multiple columns
-    Book.findAll({order: [["createdAt", "DESC"]]}).then((books) => {
-        console.log(books);
-        res.render('index', { books: books })
+    res.redirect('/books')
+});
+
+// /books render books table
+router.get('/books', (req, res) => {
+    Book.findAll({ order: [['createdAt', 'DESC']] }).then((books) => {
+        res.render('index', {books: books, title: 'books' });
     })
 });
 
-router.get('/new_book', (req, res) => {
-    res.render('new_book', { book: Book.build() });
+// /books/new render new book form
+router.get('/books/new', (req, res) => {
+    res.render('new-book', { book: Book.build(), title: 'New book' });
 });
 
 // Create new book
-router.post('/new_book', (req, res) => {
-    console.log(req.body)
+// When a post method comes in on the /books/new route
+router.post('/books/new', (req, res) => {
+    // Sequelize create book with the data in req.body
     Book.create(req.body).then( (book)=> {
-        console.log(book);
-        res.redirect("/book_detail/" + book.id)
-    }).catch(error => console.log(error));
-});
-
-// Get specific book
-router.get('/book_detail/:id', (req, res) => {
-    console.log('id', req.params.id);
-    Book.findById(req.params.id).then((book) => {
-        console.log(book);
-        res.render('book_detail', { book: book });
+        // then redirect to spcific book route
+        res.redirect("/books/" + book.id)
     }).catch((error) => console.log('We got an error', error));
 });
 
-// Update
-router.post('/book_detail/update/:id', (req, res) => {
+// /books/:id render book with requested id
+router.get('/books/:id', (req, res) => {
+    // Sequelize find book by id
     Book.findById(req.params.id).then((book) => {
+        // then render update book from
+        res.render('update-book', { book: book, title: 'Update book' });
+    }).catch((error) => console.log('We got an error', error));
+});
+
+// Update book
+// When a post methiod comes in on the /books/:id route
+router.post('/books/:id', (req, res) => {
+    // Sequelize find book by id
+    Book.findById(req.params.id).then((book) => {
+        //  Sequelize update the book
         return book.update(req.body);
     }).then((book) => {
-        res.redirect('/book_detail/' + book.id);
+        // Then redirect to /books/:id route
+        res.redirect('/books/' + book.id);
     }).catch((error) => console.log('We got an error', error));
 });
 
-// Delete
+// Delete book
 router.post('/books/:id/delete', (req, res) => {
+    //  Sequelize find book by id
     Book.findById(req.params.id).then((book) => {
+        // Sequelize Destroy book
         return book.destroy();
     }).then((book) => {
-        res.redirect('/');
+        // then redirect to /books route
+        res.redirect('/books');
     }).catch((error) => console.log('We got an error', error));
 });
 
