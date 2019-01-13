3d585ffcb7830f0123ba2c580576931b926c9063
Jeffrey Meesters <jjeffreymeesters@gmail.com>
2019-01-13 17:09:36 +0100

    Add create new book, get single book, get all books and parse data into views

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const sequelize = require('./models').sequelize;
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+
 const host ='localhost';
 const port = 3000;
 
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const Book = require("../models").Book;
 
 router.get('/', (req, res) => {
-    res.render('index')
+    // FindAll accepts an array of arrays as options
+    // This way you can order on multiple columns
+    Book.findAll({order: [["createdAt", "DESC"]]}).then((books) => {
+        console.log(books);
+        res.render('index', { books: books })
+    })
 });
 
 router.get('/new_book', (req, res) => {
-    res.render('new_book')
+    res.render('new_book', { book: Book.build() });
 });
 
+// Create new book
 router.post('/new_book', (req, res) => {
-    console.log(req.params)
+    console.log(req.body)
+    Book.create(req.body).then( (book)=> {
+        console.log(book);
+        res.redirect("/book_detail/" + book.id)
+    }).catch(error => console.log(error));
+});
+
+// Get specific book
+router.get('/book_detail/:id', (req, res) => {
+    console.log('id', req.params.id);
+    Book.findById(req.params.id).then((book) => {
+        console.log(book);
+        res.render('book_detail', { book: book });
+    }).catch((error) => console.log('We got an error', error));
 });
 
 module.exports = router;
